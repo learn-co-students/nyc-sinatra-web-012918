f0c6aa290ecaaf68b5acd1b7bb89da2ef9f5e170
aalexander3 <aralx73@gmail.com>
2018-02-22 15:37:47 -0500

    waterfall

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,39 @@
 class LandmarksController < ApplicationController
+  set :method_override, true
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark_name], year_completed: params[:landmark_year_completed])
+    redirect '/landmarks'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:name], year_completed: params[:year_completed])
+    binding.pry
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
 
 
 end
