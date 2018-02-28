71399b69d5087d5ac8bb9f15a311727f73a2b6dd
github username <github email address>
2018-02-28 16:32:02 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,40 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
 
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :"/landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+    if !params["figure"]["name"].empty?
+      @landmark.figure << Figure.create(name: params["figure"]["name"])
+    end
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :"/landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params[:id])
+    @figures = Figure.all
+    erb :"/landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    @landmark.update(params["landmark"])
+    if !params["figure"]["name"].empty?
+      @landmark.figure << Figure.create(name: params["figure"]["name"])
+    end
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
 end
